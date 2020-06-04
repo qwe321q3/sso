15cce47910242ea8678d5c3c992629a23f6cccfe
tianshuo <qwe321a3@126.com>
2020-06-04 14:33:57 +0800

    优化代码，增加serviceId和loginId的绑定关系。

--- sso-server/src/main/java/com/tianshuo/beta/sso/ticket/ServiceTicketImpl.java
+++ sso-server/src/main/java/com/tianshuo/beta/sso/ticket/ServiceTicketImpl.java
@@ -1,5 +1,6 @@
 package com.tianshuo.beta.sso.ticket;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
@@ -23,11 +24,23 @@ public class ServiceTicketImpl extends AbstractTicket implements ServiceTicket, 
     public ServiceTicketImpl(LoginTicket loginTicket, String service) {
         this.loginTicket = loginTicket;
         this.service = service;
-        this.setId(generateId());
+        this.setId(generateId(loginTicket.getId()));
     }
 
     public ServiceTicketImpl() {
         this.setId(generateId());
+    }
+
+    /**
+     * ServiceId必须根据登录id来生成
+     * @param loginTicketId
+     * @return
+     */
+    private String generateId(String loginTicketId) {
+        StringBuffer stringBuffer = new StringBuffer(prx);
+        stringBuffer.append("-");
+        stringBuffer.append(UUID.fromString(loginTicketId));
+        return stringBuffer.toString();
     }
 
     private String generateId() {
@@ -74,5 +87,24 @@ public class ServiceTicketImpl extends AbstractTicket implements ServiceTicket, 
                 ", service='" + service + '\'' +
                 ", id='" + this.getId() + '\'' +
                 '}';
+    }
+
+    public static void main(String[] args) {
+        String uuid = UUID.randomUUID().toString();
+
+        System.out.println(uuid);
+
+        String sid = UUID.nameUUIDFromBytes(uuid.getBytes()).toString();
+
+        String a = "68fc9fde-8991-4957-b7a1-c0ab4a6c034e";
+
+        System.out.println(sid);
+
+//        68fc9fde-8991-4957-b7a1-c0ab4a6c034e
+//        92f5d440-8970-3596-ad41-865a9fdf9354
+
+        System.out.println(UUID.nameUUIDFromBytes(a.getBytes()).toString());
+
+
     }
 }
