a03c3b8e2e8928b8297695abbc6548d32603000a
tianshuo <qwe321a3@126.com>
2020-06-07 21:34:30 +0800

    增加sql语句级流程图

---
+++ sso-server/sql/ticket.sql
@@ -0,0 +1,48 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.19 : Database - eims
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`eims` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `eims`;
+
+/*Table structure for table `service_ticket` */
+
+DROP TABLE IF EXISTS `service_ticket`;
+
+CREATE TABLE `service_ticket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ticket_id` varchar(64) NOT NULL COMMENT '登录票据id',
+  `service_ticket_id` varchar(64) DEFAULT NULL COMMENT '服务票据id',
+  `service` varchar(128) DEFAULT NULL COMMENT '服务路径',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ticket` */
+
+DROP TABLE IF EXISTS `ticket`;
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ticket_id` varchar(64) NOT NULL COMMENT '登录票据id',
+  `created_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
